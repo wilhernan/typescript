52368f246b6a1310c24a2978de4988d3f803729a
wilhernan <wilhernan777@gmail.com>
2020-07-28 13:51:03 -0400

    commit fix index.js

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express'
 import { indexController } from '../controllers/index.controller'
 
-const router: Router = Router();
+class IndexRoutes{
 
-router.get('/', indexController.index);
-router.post('/', indexController.saveInteraction);
+    public router: Router = Router();
 
-export default router;
+    constructor() {
+        this.config();
+    }
+
+    config(): void {
+        this.router.get('/', indexController.index);
+        this.router.post('/', indexController.saveInteraction);
+    }
+}
+
+
+const indexRoutes = new IndexRoutes();
+export default indexRoutes.router;
